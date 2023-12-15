2f9033fe5aa0af619b5c8ae25a09538ef4b5e1b5
angelmss12 <152249662+angelmss12@users.noreply.github.com>
2023-12-14 21:29:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function validarCURP() {
     var curp = document.getElementById("curp").value;
     var curpRepetido = document.getElementById("curpRepetido").value;
 
+    // Verificar que el CURP tenga al menos 16 caracteres
+    if (curp.length < 16) {
+        alert("El CURP debe tener al menos 16 caracteres");
+        return;
+    }
+
     if (curp === curpRepetido) {
         alert("CURP validado correctamente");
     } else {
@@ -25,6 +31,21 @@ function validarEdad() {
         alert("Debe ser mayor de 18 años para registrarse como mesero");
         document.getElementById("edad").value = "";
     }
+}
+
+// Función para limpiar el formulario
+function limpiarFormulario() {
+    // Obtener todos los elementos de formulario
+    var formElements = document.querySelectorAll('form input, form select');
+
+    // Limpiar el valor de cada elemento
+    formElements.forEach(function (element) {
+        if (element.type !== "submit" && element.type !== "button") {
+            element.value = "";
+        }
+    });
+
+    // También puedes añadir lógica específica para reiniciar elementos adicionales si es necesario
 }
 
 // Función para el mensaje de mesero registrado exitosamente
